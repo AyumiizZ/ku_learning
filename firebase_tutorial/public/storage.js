bce2e0d89a3c6726ba41c084f32f2f2e93c78464
AyumiizZ <korrawitc@gmail.com>
2018-11-07 22:46:46 +0700

    :art: add progress bar

--- firebase_tutorial/public/storage.js
+++ firebase_tutorial/public/storage.js
@@ -13,19 +13,19 @@ function upload() {
     'state_changed',
     function onProgress(snapshot) {
       let progress = (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
-      document.getElementById('progress').innerText = progress + '%';
+      document.getElementById('progress').innerHTML = `Uploading progress: <progress value="${progress}" max="100"></progress>`;
     },
     function onError(error) {
 
     },
     function onComplete() {
-      firebase.storage().ref(file.name).getDownloadURL().then(function(url){
+      firebase.storage().ref(file.name).getDownloadURL().then(function (url) {
         document.getElementById('progress').innerHTML = `Completed!! <a href="${url}">Download here</a>`
         firebase.database().ref('files').push({
           name: file.name,
           url: url,
-        })
-      })
+        });
+      });
     }
   );
 }
@@ -42,11 +42,11 @@ function cancel() {
   uploadTask.cancel()
 }
 
-firebase.database().ref('files').on('value',function(snapshot){
+firebase.database().ref('files').on('value', function (snapshot) {
   const value = snapshot.val();
   const list = Object.values(value);
   document.getElementById('file-list')
-  for(let file of list){
+  for (let file of list) {
     document.getElementById('file-list').innerHTML += `<a href="${file.url}">${file.name}</a><br>`
   }
 })
