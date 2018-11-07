a0bc9b8167716bd3011f4401ceb4568a821e56dc
AyumiizZ <korrawitc@gmail.com>
2018-11-07 17:37:38 +0700

    :construction: add set update push delete

--- firebase_tutorial/public/database.js
+++ firebase_tutorial/public/database.js
@@ -1,3 +1,31 @@
-firebase.database().ref().on('value',function(snapshot){
-    document.getElementById('realtime-data').innerText = snapshot.val()
+function setData() {
+    firebase.database().ref('profile').set({
+        name: "aon",
+        age: 20,
+        faculty: "Engineering",
+        devices: {
+            iphone: true,
+            macbook: true,
+            huawei: true
+        },
+    });
+}
+
+function updateData() {
+    firebase.database().ref('profile').update({
+        name: "khem",
+    });
+}
+
+function pushData() {
+    firebase.database().ref('profile').child('devices').push("samsung");
+}
+
+function deleteData() {
+    firebase.database().ref('profile').remove();
+}
+
+firebase.database().ref().on('value', function (snapshot) {
+    const data = snapshot.val()
+    document.getElementById('realtime-data').innerText = JSON.stringify(data, null, 2)
 });
